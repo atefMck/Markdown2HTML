ad16c7c57297e93d76045933cdffd16375c2306b
atefsama666 <mechken.atef@gmail.com>
2020-08-02 11:43:01 +0100

    Init + Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -2,10 +2,18 @@
 """Markdown to HTML"""
 from sys import argv, exit, stderr
 import os
-if len(argv) != 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=stderr)
-    exit(1)
-if not os.path.isfile(argv[1]):
-    print("Missing {}".format(argv[1]), file=stderr)
-    exit(1)
-exit(0)
+
+
+def run():
+    """Main func"""
+    if len(argv) != 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=stderr)
+        exit(1)
+    if not os.path.isfile(argv[1]):
+        print("Missing {}".format(argv[1]), file=stderr)
+        exit(1)
+    exit(0)
+
+
+if __name__ == "__main__":
+    run()
